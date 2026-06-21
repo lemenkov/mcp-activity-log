a5cc3280a7f814f2376a228f44b41c00d6af24bb
Peter Lemenkov <lemenkov@gmail.com>
2026-06-21 09:51:53 +0200

    Make it REUISE compliant
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- src/mcp_activity/database.py
+++ src/mcp_activity/database.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2026 Peter Lemenkov <lemenkov@gmail.com>
+# SPDX-License-Identifier: Apache-2.0
+
 """SQLite database for activity logs."""
 import aiosqlite
 from datetime import datetime
